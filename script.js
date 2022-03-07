d4440f2ada78e64765d52b9f2be813ece30f7944
Kelvin Ngo <Kelvin.Ngo@brightstar.com>
2022-03-07 17:53:23 +0700

    07/03/2022 05:53 PM

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const bar = document.getElementById('bar');
+const closebar = document.getElementById('close');
 const nav = document.getElementById('navbar');
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('nav-active');
+    })
+}
+
+if (closebar) {
+    closebar.addEventListener('click', () => {
+        nav.classList.remove('nav-active');
     })
 }
